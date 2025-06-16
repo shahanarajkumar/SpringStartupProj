b39ced372128b9fa648144ee1b4726f2e4fb21c5
shahanarajkumar <shahanalav@gmail.com>
2025-06-16 23:15:08 +0200

    Initial commit

---
+++ src/main/java/com/kfg/App.java
@@ -0,0 +1,19 @@
+package com.kfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 20;
+        int b = 30;
+        int c = a+b;
+        System.out.println( a +"+"+ b +"=" + c );
+    }
+}
